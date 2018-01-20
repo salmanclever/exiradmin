5c603cb3869038fbf189fd4c044abb28e40be966
salmanclever <salmangbox@gmail.com>
2018-01-20 02:06:21 -0800

    translate filed

--- resources/lang/fa/auth.php
+++ resources/lang/fa/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'نام کاربری یا کلمه عبور صحیح نمی باشد.',
+    'throttle' => 'تلاش بیش از حد نا موفق .  :seconds دیگر مجدد تلاش کنید.',
 
 ];
