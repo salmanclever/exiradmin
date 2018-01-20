e0b60930e2064f87453f23fbe66df07e56132e0a
salmanclever <salmangbox@gmail.com>
2018-01-20 02:06:55 -0800

    add sidebar menu link

--- resources/views/vendor/backpack/base/inc/sidebar.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar.blade.php
@@ -14,6 +14,12 @@
           <!-- ================================================ -->
           <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
 
+          <li><a href="{{ backpack_url('employee') }}"><i class="fa fa-users"></i> <span>{{ trans('backpack::base.employee') }}</span></a></li>
+
+              <li><a href="{{ backpack_url('department') }}"><i class="fa  fa-cube"></i> <span>{{ trans('backpack::base.department') }}</span></a></li>
+              <li><a href="{{ backpack_url('group') }}"><i class="fa   fa-object-group"></i> <span>{{ trans('backpack::base.group') }}</span></a></li>
+              <li><a href="{{ backpack_url('categorie') }}"><i class="fa  fa-object-ungroup"></i> <span>{{ trans('backpack::base.categorie') }}</span></a></li>
+
 
               <li><a href="{{  backpack_url('elfinder') }}"><i class="fa fa-files-o"></i> <span>{{ trans('backpack::base.file_manager') }}</span></a></li>
 
