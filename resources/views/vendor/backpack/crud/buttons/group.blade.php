e3fa7dce8fecd7dd1cedf10af7c8f4ff7194c99c
salmanclever <salmangbox@gmail.com>
2018-01-24 15:45:25 +0330

    group view in cat

--- resources/views/vendor/backpack/crud/buttons/group.blade.php
+++ resources/views/vendor/backpack/crud/buttons/group.blade.php
@@ -2,21 +2,21 @@
     @if (!$crud->model->translationEnabled())
 
         <!-- Single edit button -->
-        <a href="{{ url($crud->route.'/'.$entry->getKey().'/addemployee') }}" class="btn btn-xs btn-default"><i class="fa  fa-user-plus"></i> {{ trans('backpack::crud.add_employee') }}</a>
+        <a href="{{ url($crud->route.'/'.$entry->getKey().'/addgroup') }}" class="btn btn-xs btn-default"><i class="fa  fa-user-plus"></i> {{ trans('backpack::crud.add_group') }}</a>
 
     @else
 
         <!-- Edit button group -->
         <div class="btn-group">
-            <a href="{{ url($crud->route.'/'.$entry->getKey().'/addemployee') }}" class="btn btn-xs btn-default"><i class="fa fa-edit"></i> {{ trans('backpack::crud.add_employee') }}</a>
+            <a href="{{ url($crud->route.'/'.$entry->getKey().'/addgroup') }}" class="btn btn-xs btn-default"><i class="fa fa-edit"></i> {{ trans('backpack::crud.add_group') }}</a>
             <button type="button" class="btn btn-xs btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="caret"></span>
                 <span class="sr-only">Toggle Dropdown</span>
             </button>
             <ul class="dropdown-menu dropdown-menu-right">
-                <li class="dropdown-header">{{ trans('backpack::crud.add_employee') }}:</li>
+                <li class="dropdown-header">{{ trans('backpack::crud.add_group') }}:</li>
                 @foreach ($crud->model->getAvailableLocales() as $key => $locale)
-                    <li><a href="{{ url($crud->route.'/'.$entry->getKey().'/addemployee') }}?locale={{ $key }}">{{ $locale }}</a></li>
+                    <li><a href="{{ url($crud->route.'/'.$entry->getKey().'/group') }}?locale={{ $key }}">{{ $locale }}</a></li>
                 @endforeach
             </ul>
         </div>
