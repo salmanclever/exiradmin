aaf2b2cb55e48520f5b06e2cb4a2226081008c89
salmanclever <salmangbox@gmail.com>
2018-02-26 15:46:51 +0330

    Procfile for Heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         //
         Message::creating(function($message){
            $message->creator_id = Auth::id();
